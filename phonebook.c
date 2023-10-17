5b1e480dc0a1bf0773b67c36ec1460abe20c284f
CnhawkinsSVS <147911290+CnhawkinsSVS@users.noreply.github.com>
2023-10-17 06:20:00 +0000

    Tue, Oct 17, 2023, 6:20 AM +00:00

--- phonebook.c
+++ phonebook.c
@@ -5,5 +5,5 @@ int main(void)
 {
     string name = get_string("what's your name? ");
     int age = get_int("How old are you? ");
-    int  = get_string(" What's your phone number? \n");
+    long number = get_long("What's your phone number? \n");
 }
