d717d52cab4c57990dc12f885ff6588ff81b35ad
CnhawkinsSVS <147911290+CnhawkinsSVS@users.noreply.github.com>
2023-10-17 07:06:11 +0000

    Tue, Oct 17, 2023, 7:06 AM +00:00

--- population.c
+++ population.c
@@ -3,6 +3,12 @@
 
 int main(void)
 {
-    //Prompt user for a starting number
-    
+    //Prompt user for a starting # of lamas
+
+    //Prompt user for an ending # of lamas
+
+    // (Loop) How many years will it take to get to goal?
+    // Every year 1/3 lamas born; 1/4 pass away
+
+
 }
