c2e75e495103e0e8bc39717bfe5e7e22f7feb6cf
CnhawkinsSVS <147911290+CnhawkinsSVS@users.noreply.github.com>
2023-10-17 07:22:45 +0000

    Tue, Oct 17, 2023, 7:22 AM +00:00

--- population.c
+++ population.c
@@ -4,14 +4,15 @@
 int main(void)
 {
     //Prompt user for a starting # of lamas
-    long number = get_long("What's the starting number of lamas? ")
+    long number = get_long("Starting size: ");
 
     //Prompt user for an ending # of lamas
-    long number = get_long("What's the end number of lamas? ")
+    long int = get_long("End size: ");
 
     // (Loop) How many years will it take to get to goal?
     // Every year 1/3 lamas born; 1/4 pass away
-    
+    int year = get_int("Years: \n");
+
 
 
 }
