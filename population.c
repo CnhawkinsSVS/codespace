72e181f39b1830ff34db27fdfe3bba0b820fbfe3
CnhawkinsSVS <147911290+CnhawkinsSVS@users.noreply.github.com>
2023-10-17 07:34:02 +0000

    Tue, Oct 17, 2023, 7:34 AM +00:00

--- population.c
+++ population.c
@@ -4,11 +4,7 @@
 int main(void)
 {
     //Prompt user for a starting # of lamas
-    string start = get_string("Starting size: ");
-    {
-        const int n =12;
-        for (int i = 0; i)
-    }
+    int start = 
 
     //Prompt user for an ending # of lamas
     string end = get_string("End size: ");
