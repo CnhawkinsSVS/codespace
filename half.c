05d30c1555284b568513b896ceb97d94ab44f798
CnhawkinsSVS <147911290+CnhawkinsSVS@users.noreply.github.com>
2023-10-18 08:55:47 +0000

    Wed, Oct 18, 2023, 8:55 AM +00:00

--- half.c
+++ half.c
@@ -16,12 +16,12 @@ int main(void)
 }
 
 // TODO: Complete the function
-float half(float bill, float tax, int tip)
+float half(float bill, float tax, int tip);
 {
-    float tax_amount = bill * (tax/100);
-    float total_1 = bill + tax_amount;
-    float tip_amount = total_1 * (tip/100);
-    float total_2 = total_1 + tip_amount;
-    float half = total_2 / 2;
+    tax_amount = bill * (tax/100);
+    total_1 = bill + tax_amount;
+    tip_amount = total_1 * (tip/100);
+    total_2 = total_1 + tip_amount;
+    half = total_2 / 2;
     return half;
 }
